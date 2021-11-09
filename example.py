19cc833d075821153d7795994de80dc3180d4146
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2021-11-08 22:09:39 -0500

    first commit

---
+++ example.py
@@ -0,0 +1,19 @@
+from textblob import TextBlob
+import sys
+import tweepy
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+import nltk
+import pycountry
+import re
+import string
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from langdetect import detect
+from nltk.stem import SnowballStemmer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import CountVectorizer
+
